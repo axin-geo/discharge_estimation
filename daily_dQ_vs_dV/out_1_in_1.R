6b0a7eba4702ee0fc1c5ca40d96af432897ead71
Aote Xin <aote.umwelt@outlook.com>
2020-07-13 01:11:05 -0500

    20200713 01:10 Q_est, Q_obs, V_obs, V_et_obs

--- daily_dQ_vs_dV/out_1_in_1.R
+++ daily_dQ_vs_dV/out_1_in_1.R
@@ -115,10 +115,9 @@ head(et_day, 6)
 #########################
 
 # This code chunk rescales daily discharge difference and storage variation derived from previous codes by a time period of R (a variable representing SWOT time scale)
-# Note: Using daily V and V_et rather than daily dV to calculate rescaled dV and V_et (in case some daily dV has missing in situ observations)
 # Note: dQ in a unit of millin m3/day, dV in a unit of million m3
 
-# Define a function 'mx' that returns a matrix simulating sampling scenarios. ***Required input: 'colname_mx' as column names in the returned matrix
+# Define a function 'mx' that returns a matrix simulating sampling scenarios. ***Required input as a charactor string: 'colname_mx' as column names in the returned matrix
 mx <- function(colname_mx){
   mx <- matrix(nrow = nrow(y));
   for (i in 1:R) {
@@ -139,16 +138,19 @@ mx <- function(colname_mx){
   return(mx) 
 }
 
-###################################
-# Rescale: discharge data ('Q_est')
+###############################################################################################
+# Rescale: discharge data ('Q_est') ###########################################################
+# NOTE:We used daily discharge difference to derive discharge difference in a R-day timescale #
+###############################################################################################
+
 Q <- y %>% select(., datetime, dQ) 
 
 # A matrix object simulating sampling scenarios
-Q_est <- mx('Q')
+Q_est <- mx('dQ')
 
 Q_est <- cbind(Q, Q_est)
 
-# Aggregate daily discharge difference to volumeric unit
+# Aggregate daily discharge difference to a volumeric unit
 # NOTE: dQ in a unit of millin m3/day, Q in a unit of million m3
 for (i in 1:R){
   for (j in 1:((nrow(Q_est) - i) %/% R)){
@@ -166,176 +168,81 @@ for (i in 1:R){
   }
 }
 
-#################################
-# Rescale: storage data ('V_obs' and 'V_et_obs')
-V <- y %>% select(., datetime, dV, V) %>% left_join(., et_day)
+###################################################################################################################################
+# Rescale: storage data ('V_obs') ##################################################################################
+# NOTE: We used daily storage observations rather than daily storage variations to derive storage variations in a R-day timescale #
+###################################################################################################################################
+
+V <- y %>% select(., datetime, V) %>% left_join(., et_day)
 
 # A matrix object simulating sampling scenarios
-V_obs <- mx('V')
+V_obs <- mx('dV')
 
 V_obs <- V %>% select(., -ET) %>% cbind(., V_obs)
 
-# Assignment of storage observations in different sampling scenarios
-
 # Calculate the storage variation in different sampling scenarios
-
-# Aggregate daily discharge difference to volumeric unit
-# NOTE: dQ in a unit of millin m3/day, Q in a unit of million m3
 for (i in 1:R){
-  for (j in 1:((nrow(Q_est) - i) %/% R)){
-    a <- R*(j - 1) + i + 1; b <- j*R + i;
-    
-    if(j == 1){
-      Q_est[i, i + 2] <- sum(Q_est[, 2][1:i]);
-      Q_est[j*R + i, i + 2] <- sum(Q_est[, 2][a:b]);
-      Q_est[,i + 2][a:(b - 1)] <- NA;
-    } 
-    else{
-      Q_est[j*R + i, i + 2] <- sum(Q_est[, 2][a:b]);
-      Q_est[,i + 2][a:(b - 1)] <- NA;
+  for (j in 1:nrow(V_obs)){
+    if(j == i){
+      V_obs[j, i + 2] <- NA
+    }
+    else if((j - i) %% R == 0){ 
+      V_obs[j, i + 2] <- V_obs[j, 2] - V_obs[j - R, 2]
     }
   }
 }
 
+###################################################################################################################################
+# Rescale: storage data added with ET ('V_et_obs') ##################################################################################
+# NOTE: We used daily storage observations rather than daily storage variations to derive storage variations in a R-day timescale #
+###################################################################################################################################
 
+# A matrix object simulating sampling scenarios
+V_et_obs <- mx('dV_et')
 
+V_et_obs <- V %>% mutate(., V_et = V + ET) %>% select(., datetime, V_et) %>% cbind(., V_et_obs)
 
-
-
-
-
-
-
-
-
-
-dy <- y %>% select(., datetime, dQ, dV, V) %>% left_join(., et_day) %>% mutate(., V_R = NA, dV_R = NA, V_et = V + ET, V_et_R = NA, dV_et_R = NA) 
-head(dy,15)
-
-# dQ_R (when i = 1, 12<-[2:12])
-#i <- 1; 
-#while (i*R < nrow(dy)){
-#  dy$dQ_R[R*i + 1] <- sum(dy$dQ[(2 + i*R - R):(i*R + 1)], na.rm = T); 
-#  i <- i + 1
-#  }
-
-# V_R (Observation of storage every R days)
-dy$V_R[1] <- dy$V[1]; 
-for (i in 2:(nrow(dy) - 1)){
-  if(i %% R == 0) 
-    dy$V_R[i + 1] <- dy$V[i + 1]
+# Calculate the storage variation in different sampling scenarios
+for (i in 1:R){
+  for (j in 1:nrow(V_et_obs)){
+    if(j == i){
+      V_et_obs[j, i + 2] <- NA
+    }
+    else if((j - i) %% R == 0){ 
+      V_et_obs[j, i + 2] <- V_et_obs[j, 2] - V_et_obs[j - R, 2]
+    }
   }
-
-# dV_R (Storage variation every R days)
-i <- 1; 
-while (i*R < nrow(dy)){
-  dy$dV_R[i*R + 1] <- (dy$V_R[i*R + 1] - dy$V_R[(i - 1)*R + 1]); 
-  i <- i + 1
-  }
-
-# V_et_R ((Observation of storage added with ET every R days))
-dy$V_et_R[1] <- dy$V_et[1];
-for (i in 2:(nrow(dy) - 1)){
-  if(i %% R == 0) 
-    dy$V_et_R[i + 1] <- dy$V_et[i + 1]
-  }
-
-# dV_et_R
-i <- 1;
-while (i*R < nrow(dy)){
-  dy$dV_et_R[i*R + 1] <- (dy$V_et_R[i*R + 1] - dy$V_et_R[(i - 1)*R + 1]); 
-  i <- i + 1
-  }
-
-# Q1:QR (Estimated discharge difference derived from daily discharge difference)
-## Binding 'mx_est' to 'dy'
-dy <- dy %>% select(-c())
-dy <- cbind(dy, mx_est)
-
-# deleting columns with daily observations
-dy <- dy %>%
-  select(datetime, dQ, dV, dQ_R, dV_R, dV_et_R)
-
-head(dy,20)
-head(y)
-
-# Plotting dQ_R vs dV_R & dV_et_R
-range_limit <- max(abs(min(min(dy$dV_R, na.rm = TRUE), min(dy$dQ_R, na.rm = TRUE))), abs(max(max(dy$dV_R, na.rm = TRUE), max(dy$dQ_R, na.rm = TRUE))))
-ggplot(dy) +
-  geom_point(mapping = aes(x = dQ_R, y = dV_R), color="darkgreen", shape= 17, alpha = 1/2, size = 2, na.rm = TRUE) +
-  geom_segment(aes(x = min(min(dy$dV_R, na.rm = TRUE), min(dy$dQ_R, na.rm = TRUE)), y = min(min(dy$dV_R, na.rm = TRUE), min(dy$dQ_R, na.rm = TRUE)), xend = max(max(dy$dV_R, na.rm = TRUE), max(dy$dQ_R, na.rm = TRUE)), yend = max(max(dy$dV_R, na.rm = TRUE), max(dy$dQ_R, na.rm = TRUE))),linetype = "dashed") +
-  geom_point(mapping = aes(x = dQ_R, y = dV_et_R), color="red", shape= 17, alpha = 1/2, size = 2, na.rm = TRUE) +
-  labs(title =paste0("Storage-discharge balance for \n", s_name, " \n(Sampling Gap: ", R, " days)"), x = "dQ (million m^3)", y = "dV (million m^3)") +
-  xlim(-range_limit, range_limit) + ylim(-range_limit, range_limit) +
-  stat_smooth(aes(dy$dQ_R, dy$dV_et_R), method = "lm", col = "red", se = FALSE)
-
-# Create a dataframe object 'dy_R' with observations at a R-day timescale
-dy_R <- dy %>% xts(., order.by = .$datetime) %>% subset(., select = -c(datetime, dQ, dV))
-
-
-#############################################
-# Uncertainty analysis due to sampling gaps #
-#############################################
-
-# A loop to create a matrix (mx) of sampling days in different scenarios
-mx <- matrix(nrow = nrow(dy));
-for (i in 1:R) {
-  ## adding R-1 rows of NA of sequence
-  r <- rbind(seq(from = i, to = nrow(y), by = R), matrix(ncol = length(seq(from = i, to = nrow(y), by = R)), nrow = R - 1)); 
-  ## adding i-1 of NA before the first value of the vector
-  r_NA <- c(rep(NA, i - 1), r);
-  ## length correction 
-  length(r_NA) <- nrow(y);
-  
-  mx <- as.data.frame(cbind(mx, r_NA)); 
-  names(mx)[i + 1] <- paste0(c("dQ"), i)
 }
 
-# drop defaut column from df
-mx <- subset(mx, select = -V1) 
+#############################################################################
+# Simulating observed discharge differences by a SWOT's timescale ('Q_obs') #
+#############################################################################
+# Here we simulate the effects of sampling gaps and assumed discharge difference on days without measurements takes on a linear distribution btw sampling days.
+# A matrix object simulating sampling scenarios
+Q_obs <- mx('RdQ');
+Q_obs <- cbind(Q, Q_obs)
 
-# convert mx to time series object
-mx <- zoo(mx, dy$datetime) 
-dy <- dy  %>% xts(., order.by = .$datetime) %>% subset(., select = -c(datetime, dV_et_R))
-
-# add number of days as a new column
-day <- seq(from = 1, to = nrow(dy)); dy <- cbind(dy, day)  
-
-# join indices of sampling days (mx) to dy 
-dy <- merge(dy, mx)
-
-###############################################################
-# Unit conversion of discharge difference from flow to volume #
-###############################################################
-
-# Due to sampling gaps, discharge difference can only be calculated every R days by SWOT.
-# Here we simulate the effects of sampling gaps and assume discharge difference on days without measurements takes on a linear distribution btw sampling days.
-
-# Assign discharge difference values in previously derived sequences of R scenarios
+# Assign discharge difference values in different sampling scenarios
 for (i in 1:R){
-  for (j in 1:nrow(dy)){
-    if (!is.na(dy[j, i + 5]))
-      dy[j, i + 5] <- dy[j, 1]
-  }}
+  for (j in 1:nrow(Q_obs)){
+    if (!is.na(Q_obs[j, i + 2])){
+      Q_obs[j, i + 2] <- Q_obs[j, 2]
+      }
+    }
+}
 
 # Linearly interpolate the discharge differences on days without measurements
-dy <- na_interpolation(dy, option = "linear") %>% subset(., select = -c(day,dQ,dV))
+Q_obs <- na_interpolation(Q_obs, option = "linear") %>% select(., -dQ)
 
 # Aggregate daily dQ and dV by a time period of R (representing SWOT time scale)
 # dQ in a unit of millin m3/day
 for (i in 1:R){
-  for (j in 1:((nrow(dy)-i)%/%R)){
-    a <- R*(j - 1) + i + 1; b <- j*R + i
-    dy[j*R + i, i + 2] <- sum(dy[,i+2][a:b])
-    dy[,i+2][a:(b-1)] <- NA
+  for (j in 1:((nrow(Q_obs)-i) %/% R)){
+    a <- R*(j - 1) + i + 1; b <- j*R + i;
+    Q_obs[j*R + i, i + 1] <- sum(Q_obs[, i + 1][a:b]);
+    Q_obs[, i + 1][a:(b - 1)] <- NA
   }
 }
-
-
-# unify the time steps for discharge simulation with #R scenarios
-QR <- dy %>%
-  subset(., select = -c(dV_R, dQ_R))
-
 
 # plot the hydrograph
 
